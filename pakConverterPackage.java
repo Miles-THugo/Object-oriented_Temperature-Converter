ca9e8d59919eab68252051df2f81e920d2425457
Miles-THugo <95313075+Miles-THugo@users.noreply.github.com>
2022-02-20 05:15:27 +0000

    Create pakConverterPackage.java

---
+++ pakConverterPackage.java
@@ -0,0 +1,16 @@
+package converterPackage;
+
+/**
+ * @author Miles (amn35)
+ */
+public class pakConverterPackage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConverterGUI gui = new ConverterGUI();
+        gui.setVisible(true);
+    }
+    
+}
